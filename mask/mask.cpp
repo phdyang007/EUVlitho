c347f8c079fc882a8144169d27da15dfa694de26
tanabe <tanabe.h.af@m.titech.ac.jp>
2024-11-19 17:23:50 +0900

    add emint

---
+++ mask/mask.cpp
@@ -0,0 +1,22 @@
+#include<fstream>
+#include <cstdlib>
+#include <cmath>
+using namespace std;
+
+#include "./LSBF.h"
+
+int main (int argc,char* argv[])
+{
+ ofstream ofs("mask.csv");
+ int NDIVX =1024;
+ int NDIVY =NDIVX;
+ int NDIVSQ=NDIVX*NDIVY;
+ int* mask2d=new int[NDIVSQ];
+ maskgen(mask2d,NDIVX,NDIVY);
+ for(int i=0;i<NDIVX;i++)
+ for(int j=0;j<NDIVY;j++)
+   ofs<<mask2d[NDIVY*i+j]<<",";
+ ofs<<endl;
+ return 0;
+}
+
